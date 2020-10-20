178a551ac31c000557e6fbe9dde8878309eadd3d
Harmacistant <william.mv.h@gmail.com>
2020-10-20 14:28:44 -0500

    added options for character and word count goals; removed visual borders between text area and status bar label; adjusted menubar for macOS to remove default values.

--- PyWrite.py
+++ PyWrite.py
@@ -1,20 +1,39 @@
-from platform import system as platform_sys
+import platform
 import re
 from tkinter import *
 
 
 class PyWrite:
     __root = Tk()
+    __root.attributes("-fullscreen", True)
+    __root.minsize(400, 400)
+    __thisFullscreenState = False
     __thisWidth = 500
     __thisHeight = 700
-    __thisTextArea = Text(__root, font=('*Font', '17'))
+    __thisTextArea = Text(__root,
+                          font=('*Font', '14'),
+                          padx=0,
+                          pady=0,
+                          borderwidth=0,
+                          highlightthickness=0,
+                          relief=FLAT,
+                          wrap=WORD)
     __thisScrollBar = Scrollbar(__thisTextArea)
-    __thisStatusBar = Label(__root)
+    __thisStatusBar = Label(__root,
+                            font=('*Font', '14'),
+                            padx=0,
+                            pady=0,
+                            borderwidth=0,
+                            highlightthickness=0,
+                            relief=FLAT)
     __thisMenuBar = Menu(__root)
     __thisAppMenu = Menu(__thisMenuBar, name='apple')
     __thisMenuBar.add_cascade(menu=__thisAppMenu)
     __file = None
     __charCount = 0
+    __charCountGoal = None
+    __wordCount = 0
+    __wordCountGoal = None
 
     def __init__(self, **kwargs):
         # icon
@@ -26,12 +45,15 @@ class PyWrite:
         # Foreground and Background
         try:
             self.__thisTextArea['background'] = kwargs['bgcolor']
+            self.__root['background'] = kwargs['bgcolor']
+            self.__thisStatusBar['background'] = kwargs['bgcolor']
             if kwargs['bgcolor'] == "black":
                 self.__thisTextArea['insertbackground'] = 'white'
         except KeyError:
             pass
         try:
             self.__thisTextArea['foreground'] = kwargs['fgcolor']
+            self.__thisStatusBar['foreground'] = kwargs['fgcolor']
         except KeyError:
             pass
 
@@ -59,20 +81,21 @@ class PyWrite:
         self.__root.grid_columnconfigure(0, weight=1)
 
         # Add Controls
-        self.__thisTextArea.grid(sticky=N + E + S + W)
+        self.__thisTextArea.grid(sticky=N + E + W + S)
         self.__thisStatusBar.grid(sticky=W + S)
         self.__thisScrollBar.config(command=self.__thisTextArea.yview)
         self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
 
-        if platform_sys() == 'Darwin':
+        if platform.system() == 'Darwin':
             # Add Apple Menubar
-            # see https://tkdocs.com/tutorial/menus.html
+            # this still shows as 'Python' by default, instead of PyWrite.
+            # according to https://tkdocs.com/tutorial/menus.html, renaming the binary should fix this
             self.__root['menu'] = self.__thisMenuBar
-        elif platform_sys() == 'Linux':
+        elif platform.system() == 'Linux':
             # Linux Menu Options Here
             pass
         else:
-            # Presume Windows Options by Default
+            # Windows Menu Options by Default
             pass
 
         # Add Commands
@@ -82,14 +105,29 @@ class PyWrite:
         # Set title
         self.__root.title("PyWrite - [New Document]")
 
-    def __ctrl(self, event):
+    def __toggleFullscreen(self, event=None):
+        self.__thisFullscreenState = not self.__thisFullscreenState
+        self.__root.attributes("-fullscreen", self.__thisFullscreenState)
+        return "break"
+
+    def __ctrl(self, event=None):
         print("Still trying to figure out how control bindings work...")
         pass
 
-    def __key(self, event):
-        self.__charCount = len(self.__thisTextArea.get("1.0", "end-1c"))
-        self.__thisStatusBar['text'] = self.__charCount
-        pass
+    def __key(self, event=None):
+        if event:
+            pass
+        allText = self.__thisTextArea.get("1.0", "end-1c")
+        self.__charCount = len(allText)
+        self.__wordCount = len(allText.strip().split(" "))
+
+        charCountLabel = f"{self.__charCount}" if not self.__charCountGoal \
+            else f"{self.__charCount} / {self.__charCountGoal}"
+
+        wordCountLabel = f"{self.__wordCount}" if not self.__wordCountGoal \
+            else f"{self.__wordCount} / {self.__wordCountGoal}"
+
+        self.__thisStatusBar['text'] = charCountLabel + "\t" + wordCountLabel
 
     def __quit(self):
         self.__root.destroy()
